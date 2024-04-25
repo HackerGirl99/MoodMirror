116aaae084ed83aa7421ca443657a4e201a6fd33
HackerGirl99 <asnaniyas98@gmail.com>
2024-04-25 13:13:33 +0530

    Frontend uploaded

---
+++ MoodMirrorApp/App.js
@@ -0,0 +1,95 @@
+
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from 'react-native-vector-icons'; // Import icons library
+
+const Tab = createBottomTabNavigator();
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+};
+
+const AnalyzeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Analyze your emotions here!</Text><br></br>
+      <View>
+      <View style={styles.cameraButtonContainer}>
+       <Button title="Open Your Camera" onPress={() => { /* navigate to camera screen */}} />
+       </View><br></br>
+      </View>
+    </View>
+  );
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Analyze') {
+              iconName = focused ? 'video' : 'video-outline';
+            }
+
+            return <MaterialCommunityIcons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Analyze" component={AnalyzeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  navBar: {
+    height: 50,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cameraButtonContainer: {
+    backgroundColor: 'transparent',
+  },
+  cameraButton: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: '#3498db', // Customize button background color
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: 50,
+    backgroundColor: '#f0f0f0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
